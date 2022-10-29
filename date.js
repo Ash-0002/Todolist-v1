2c34085221e9506edf6bbf997e4d10fc996d7b65
ashish <rashish1313@gmail.com>
2022-10-29 15:24:43 +0530

    TodoList-v1

---
+++ date.js
@@ -0,0 +1,28 @@
+
+exports.getDate = () => {
+    
+const today = new Date();
+
+const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
+
+ return today.toLocaleDateString("en-Us", options);
+
+}
+
+exports.getDay = function() {
+    
+    const today = new Date();
+    
+    const options = {
+        weekday: "long",
+         
+    };
+    
+    return today.toLocaleDateString("en-Us", options);
+ 
+}
+
